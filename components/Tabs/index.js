e12ad362d77f46cccef688ca992d2137f9f807b3
ryanp <ryanpanimation@gmail.com>
2020-03-20 20:40:43 -0500

    add topics tabs

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,24 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+const createTab = topic => {
+  const tab = document.createElement("div");
+  tab.classList.add("tab");
+  tab.textContent = topic;
+
+  return tab;
+};
+const topics = document.querySelector(".topics");
+axios
+  .get("https://lambda-times-backend.herokuapp.com/topics")
+  .then(res => {
+    console.log(res.data.topics);
+    const data = res.data.topics;
+    data.forEach(item => {
+      topics.append(createTab(item));
+    });
+  })
+  .catch(err => {
+    console.log("Error: ", err);
+  });
